b86ae79c22415e04679363a001675d3ae4bc6061
Darius Bacon <darius@wry.me>
2014-06-06 23:06:22 -0700

    anagram explorer

---
+++ anagranny.py
@@ -0,0 +1,183 @@
+"""
+Produce all the dictionary words that can go into an anagram of
+the input text. Then take user's choice of a word and show the
+anagrams using it.
+
+Bare start; this could get lots fancier.
+"""
+
+import re, string, textwrap
+import sturm
+
+# Configure me by editing these constants:
+dict_filename = 'wordlist.txt'
+
+def main(argv):
+    source = ' '.join(argv[1:]).lower()
+    global dictionary, dictionary_prefixes
+    dictionary, dictionary_prefixes = load(dict_filename, source)
+    with sturm.cbreak_mode():
+        sturm.render('Collecting words...')
+        run(collect_words(source))
+
+def collect_words(source):
+    return sorted((word, rest)
+                  for name,rest in gen_anagram_words(source)
+                  for word in dictionary[name])
+    
+def run(words):
+    lines = [line.split() for line in textwrap.wrap(' '.join(word for word,_ in words),
+                                                    sturm.COLS)]
+    pos = 0
+    while True:
+        sturm.render(view_words(lines, pos))
+        key = sturm.get_key()
+        if   key == sturm.esc: return
+        elif key == '\n':      run_anagrams(words[pos])
+        elif key == 'left':    pos = (pos - 1) % len(words)
+        elif key == 'right':   pos = (pos + 1) % len(words)
+
+def view_words(lines, pos):
+    i = 0
+    for y, line in enumerate(lines):
+        if y == sturm.ROWS-1: break
+        for x, word in enumerate(line):
+            if x: yield ' '
+            if i == pos: yield sturm.cursor
+            yield word
+            i += 1
+        yield '\n'
+
+def run_anagrams((word, rest)):
+    anagrams = extend((word,), '', rest, '')
+    anagrams = list(anagrams) + [['--done--']]
+    while True:
+        sturm.render(view_anagrams(anagrams))
+        key = sturm.get_key()
+        if   key == sturm.esc: return
+
+def view_anagrams(anagrams):
+    y = 0
+    for anagram in anagrams:
+        for words in cross_product([dictionary[p] for p in anagram[1:]]):
+            if y == sturm.ROWS-1: break
+            yield ' '.join([anagram[0]] + words)
+            yield '\n'
+            y += 1
+
+def cross_product(lists):
+    if not lists:
+        yield []
+    else:
+        for xs in cross_product(lists[1:]):
+            for x in lists[0]:
+                yield [x] + xs
+
+def pigeonhole(word):
+    "Two words have the same pigeonhole iff they're anagrams of each other."
+    return ''.join(sorted(word))
+
+def load(filename, subject=None):
+    "Read in a word-list, one word per line. Prune it wrt subject."
+    pigeonholes = {}
+    prefixes = set()
+    identity = ''.join(map(chr, range(256)))
+    nonalpha = ''.join(set(identity) - set(string.ascii_lowercase))
+    usable = usable_pattern(subject)
+    def add(word):
+        if not usable(word): return
+        canon = word.lower().translate(identity, nonalpha)
+        hole = pigeonhole(canon)
+        pigeonholes.setdefault(hole, []).append(word)
+        for i in range(1, len(hole)+1):
+            prefixes.add(hole[:i])
+    for line in open(filename):
+        add(line.rstrip('\n'))
+    # 'a' and 'I' happen not to be in my wordlist file:
+    if 'a' not in pigeonholes: add('a')
+    if 'i' not in pigeonholes: add('I')
+    return pigeonholes, prefixes
+
+def usable_pattern(subject):
+    """Return a predicate that accepts words that could be part of an
+    anagram of subject. (But a None subject could be anything.) This
+    is to prune the dictionary to speed things up a bit."""
+    if subject is None:
+        return lambda word: True
+    alphabet = set(extract_letters(subject))
+    pattern = re.compile('([%s]|\W)+$' % ''.join(alphabet), re.I)
+    return pattern.match
+
+dictionary, dictionary_prefixes = None, None
+## dictionary, dictionary_prefixes = load(dict_filename)
+
+## pt = pigeonhole
+## pt('hel') in dictionary_prefixes, pt('hel') in dictionary
+#. (True, False)
+## pt('hello') in dictionary_prefixes, pt('hello') in dictionary
+#. (True, True)
+
+def write_anagram_words(s):
+    names = (name for name,rest in gen_anagram_words(s))
+    words = sorted(set(word for name in names for word in dictionary[name]))
+    text = ' '.join(words)
+    for line in textwrap.wrap(text):
+        print line
+
+def gen_anagram_words(s):
+    "Generate the pigeonhole names possible in anagrams of s."
+    bag = extract_letters(s)
+    return gen_names('', bag) if bag else ()
+
+def gen_names(wp, rest):
+    for letter, others in each_distinct_letter(rest):
+        wp1 = wp + letter
+        if wp1 in dictionary_prefixes:
+            if wp1 in dictionary:
+                if not others or any(extend((), '', others, '')):
+                    yield wp1, others
+            if others:
+                for result in gen_names(wp1, others):
+                    yield result
+
+def extend(acc, wp, rest, bound):
+    """Generate all the anagrams of the nonempty bag 'rest' that
+    extend acc with a word starting with wp, the remainder of wp
+    being lexicographically >= bound. As with gen_anagrams(), each
+    anagram is sorted and they appear in sorted order."""
+    for letter, others in each_distinct_letter(rest):
+        if bound[:1] <= letter:
+            wp1 = wp + letter
+            if wp1 in dictionary_prefixes:
+                bound1 = bound[1:] if bound[:1] == letter else ''
+                if not bound1 and wp1 in dictionary:
+                    acc1 = acc + (wp1,)
+                    if others:
+                        for result in extend(acc1, '', others, wp1):
+                            yield result
+                    else:
+                        yield acc1
+                if others:
+                    for result in extend(acc, wp1, others, bound1):
+                        yield result
+
+def extract_letters(s):
+    return make_bag(c for c in s.lower() if c.isalpha())
+
+def make_bag(letters):
+    return ''.join(sorted(letters))
+
+def each_distinct_letter(bag):
+    """Generate (letter, bag-minus-one-of-that-letter) for each
+    different letter in the bag."""
+    prefix = ''
+    for i, letter in enumerate(bag):
+        if 0 == i or letter != bag[i-1]:
+            yield letter, bag[:i] + bag[i+1:]
+
+## list(each_distinct_letter('eehlloo'))
+#. [('e', 'ehlloo'), ('h', 'eelloo'), ('l', 'eehloo'), ('o', 'eehllo')]
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
