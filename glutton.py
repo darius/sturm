37a1fd4d4eb424fbf0161ca553ac0fc30c099b8a
Darius Bacon <darius@wry.me>
2014-06-26 21:42:10 -0700

    amazingly crude ghosts

--- glutton.py
+++ glutton.py
@@ -2,6 +2,7 @@
 Pac-Man clone.
 """
 
+import random
 import sturm
 
 with open('glutton.maze') as f:
@@ -11,12 +12,17 @@ def main():
     with sturm.cbreak_mode():
         run()
 
+dbg_log = []
+dbg = dbg_log.append
+
 def run():
     grid = [list(line) for line in maze]
-    glutton = Agent(find_glutton(maze))
+    glutton = Agent('<', find_glutton(maze))
     glutton.place_on(grid)
+    ghosts = [make_ghost(grid) for _ in range(4)]
     while True:
-        sturm.render(view(grid))
+        sturm.render(view(grid), [(' ', x) for x in dbg_log])
+        dbg_log[:] = []
         key = sturm.get_key(0.1)
         if key == sturm.esc: break
         elif key == 'left':  glutton.face('>', left)
@@ -24,6 +30,8 @@ def run():
         elif key == 'up':    glutton.face('V', up)
         elif key == 'down':  glutton.face('^', down)
         glutton.act(grid)
+        for ghost in ghosts:
+            ghost.act(grid)
 
 left    = -1,  0
 right   =  1,  0
@@ -31,37 +39,72 @@ up      =  0, -1
 down    =  0,  1
 stopped =  0,  0
 
+headings = (left, right, up, down)
+
 def find_glutton(maze):
     for y, line in enumerate(maze):
         if 'P' in line:
             return line.index('P'), y
     assert False
 
+def make_ghost(grid):
+    while True:
+        x, y = (random.randint(0, len(grid[0])-1),
+                random.randint(0, len(grid)-1))
+        if grid[y][x] in '.':
+            ghost = Ghost((x, y))
+            ghost.place_on(grid)
+            return ghost
+
 class Agent(object):
-    def __init__(self, p):
+    def __init__(self, glyph, p):
         self.p = p
         self.v = stopped
         self.heading = stopped
-        self.glyph = '<'
+        self.glyph = glyph
+
     def place_on(self, grid):
         x, y = self.p
         grid[y][x] = self.glyph
+
     def face(self, glyph, heading):
         self.glyph = glyph
         self.heading = heading
+
     def act(self, grid):
         self.move(grid, self.heading) or self.move(grid, self.v)
+
     def move(self, grid, (dx, dy)):
         x, y = self.p
         x2, y2 = (x+dx) % len(grid[0]), (y+dy) % len(grid)
         if grid[y2][x2] in ' .o':
-            grid[y2][x2] = self.glyph
-            grid[y][x] = ' '
-            self.p = x2, y2
+            self.step(grid, x2, y2)
             self.v = dx, dy
             return True
         else:
             return False
+
+    def step(self, grid, x2, y2):
+        x, y = self.p
+        grid[y2][x2] = self.glyph
+        grid[y][x] = ' '
+        self.p = x2, y2
+
+class Ghost(Agent):
+    def __init__(self, p):
+        Agent.__init__(self, 'G', p)
+        self.upon = '.'
+        self.heading = random.choice(headings)
+
+    def act(self, grid):
+        if random.random() < 0.1:
+            self.heading = random.choice(headings)
+        Agent.act(self, grid)
+
+    def step(self, grid, x2, y2):
+        x, y = self.p
+        grid[y2][x2], grid[y][x], self.upon = self.glyph, self.upon, grid[y2][x2]
+        self.p = x2, y2
 
 def view(grid):
     for row in grid:
@@ -71,7 +114,7 @@ def view(grid):
             yield color(sep)
         yield '\n'
 
-def is_wall(c): return c not in ' .o<>V^'
+def is_wall(c): return c not in ' .o<>V^G'
 
 def color(c):
     return block if is_wall(c) else sturm.yellow(c) if c in '<>V^' else c
