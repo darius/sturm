9011b26dc0feb25861216588e17006af2b915e2e
Darius Bacon <darius@wry.me>
2014-07-04 20:23:52 -0700

    add power-pill mode (poorly)

--- glutton.py
+++ glutton.py
@@ -16,17 +16,23 @@ dbg_log = []
 dbg = dbg_log.append
 
 tick_interval = 1./10
+power_pill_interval = 15.0
 
 def run():
     grid = [list(line) for line in maze]
     glutton = Agent('<', find_glutton(maze))
     glutton.place_on(grid)
     ghosts = [make_ghost(grid) for _ in range(4)]
+    ghost_eater_until = 0
     for _ in ticking():
-        sturm.render(view(grid, ghosts),
+        eating_ghosts = time.time() <= ghost_eater_until
+        sturm.render(view(grid, ghosts, eating_ghosts),
                      [(' ', x) for x in dbg_log])
         if glutton.p in [ghost.p for ghost in ghosts]:
-            break
+            if eating_ghosts:
+                ghosts = [ghost for ghost in ghosts if ghost.p != glutton.p]
+            else:
+                break
         dbg_log[:] = []
         key = sturm.get_key(tick_interval)
         if key == sturm.esc: break
@@ -35,6 +41,8 @@ def run():
         elif key == 'up':    glutton.face('V', up)
         elif key == 'down':  glutton.face('^', down)
         glutton.act(grid)
+        if glutton.meal == 'o':
+            ghost_eater_until = time.time() + power_pill_interval
         for ghost in ghosts:
             ghost.act(grid)
 
@@ -72,6 +80,7 @@ class Agent(object):
         self.v = stopped
         self.heading = stopped
         self.glyph = glyph
+        self.meal = None
 
     def place_on(self, grid):
         x, y = self.p
@@ -82,6 +91,7 @@ class Agent(object):
         self.heading = heading
 
     def act(self, grid):
+        self.meal = None
         self.move(grid, self.heading) or self.move(grid, self.v)
 
     def move(self, grid, (dx, dy)):
@@ -96,6 +106,7 @@ class Agent(object):
 
     def step(self, grid, x2, y2):
         x, y = self.p
+        self.meal = grid[y2][x2]
         grid[y][x] = ' '
         grid[y2][x2] = self.glyph
         self.p = x2, y2
@@ -113,22 +124,25 @@ class Ghost(Agent):
     def step(self, grid, x2, y2):
         self.p = x2, y2
 
-def view(grid, ghosts):
+def view(grid, ghosts, eating_ghosts):
     rows = [list(row) for row in grid]
     for ghost in ghosts:
         x, y = ghost.p
         rows[y][x] = 'G'
     for row in rows:
         for i, c in enumerate(row):
-            yield color(c)
+            yield color(c, eating_ghosts)
             sep = ' -'[i+1<len(row) and is_wall(c) and is_wall(row[i+1])]
-            yield color(sep)
+            yield color(sep, eating_ghosts)
         yield '\n'
 
 def is_wall(c): return c not in ' .o<>V^G'
 
-def color(c):
-    return block if is_wall(c) else sturm.yellow(c) if c in '<>V^' else c
+def color(c, eating_ghosts):
+    return (block if is_wall(c)
+            else sturm.yellow(c) if c in '<>V^'
+            else sturm.cyan(c) if eating_ghosts and c == 'G'
+            else c)
 
 block = sturm.on_blue(' ')
 
